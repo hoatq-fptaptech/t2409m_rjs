332bd795fe1fba4a5cc8b82a9edba10c87a031d5
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-12-04 09:57:57 +0700

    demo state

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,17 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 export default function Home(){
+    const [x,setX] = useState(10); // x = 10 nhưng x là 1 state
+    function changeX(){
+        setX(x+1);// x = x + 1;
+    }
     return (
         <section>
             <Container>
-                <Row>
-                    <Col xs={6}>
-                    AAAA
-                    </Col>
-                    <Col>BBBB</Col>
-                </Row>
+                <h1>X = {x}</h1>
+                <Button onClick={changeX} variant="primary" type="button">Increment</Button>
+                <Button  variant="danger" type="button">Decrement</Button>
             </Container>
         </section>
     )
