fa4dcf155acd09e13e480443ddb703a6f60c0864
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-12-06 09:30:15 +0700

    demo select city

--- src/pages/features/Features.js
+++ src/pages/features/Features.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Container } from "react-bootstrap";
 
 export default function Features(){
     const [weather,setWeather] = useState({
@@ -29,8 +30,33 @@ export default function Features(){
     useEffect(()=>{
         getWeather();
     },[]);
+    function getCity(e){
+        const v = e.target.value;
+        const url = "https://api.openweathermap.org/data/2.5/weather?q="+v+"&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+        fetch(url).then(rs=>rs.json())
+        .then(rs=>{
+            const data = {
+                city: rs.name,
+                temp: rs.main.temp,
+                humd: rs.main.humidity,
+                press: rs.main.pressure,
+                wind: rs.wind.speed,
+                desc: rs.weather[0].description,
+                icon: rs.weather[0].icon
+            }
+            setWeather(data);
+        })
+    }
     return (
         <section>
+            <Container>
+                <select onChange={getCity} name="city" className="form-control">
+                    <option value="Hanoi">Hà Nội</option>
+                    <option value="Saigon">TP Hồ Chí Minh</option>
+                    <option value="Haiphong">TP Hải Phòng</option>
+                    <option value="london">Luân Đôn</option>
+                </select>
+            </Container>
             <div className="container">
                 <h1>{weather.city}</h1>
                 <h2>Nhiệt độ: {weather.temp} <sup>o</sup>C</h2>
