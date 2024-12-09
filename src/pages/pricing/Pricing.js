70f7957bdb32a0fedc48524ac22fa92177304adf
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-12-09 09:18:15 +0700

    chữa bài tập

--- src/pages/pricing/Pricing.js
+++ src/pages/pricing/Pricing.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./../../assets/css/forecast.css";
 export default function Pricing(){
+    const [forecast,setForecast] = useState([1,2,3,4,5,6,7,8]);
     return (
         <section>
-            <div className="container">
-                <h1>Pricing page.....</h1>
-            </div>
+            <Container>
+                <Row>
+                    {
+                        forecast.map((e,i)=>{
+                            return (
+                                <Col xs={3}>
+                                    <div className="item w-100">
+                                        <p className="text-time text-white">2024-12-09 09:00:00</p>
+                                        <h2 className="text-white">18<sup>o</sup>C</h2>
+                                        <p className="text-white description">Trời lạnh, ít mây</p>
+                                        <img src="https://openweathermap.org/img/wn/04d@2x.png"/>
+                                    </div>
+                                </Col>
+                            )
+                        })
+                    }                
+            
+                </Row>
+            </Container>
         </section>
     )
 }
